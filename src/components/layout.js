b8c5993f945b1011ad98fe99a4b02d6b625feb44
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2020-06-21 18:33:53 +0100

    Remove irrelevant things from default starter install

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -35,9 +33,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
