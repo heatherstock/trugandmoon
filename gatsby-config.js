45bb08084f33811cdeabb207a924136b6a2cb458
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2020-06-21 21:46:59 +0100

    Edit config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
+        path: `${__dirname}/src`,
       },
     },
     `gatsby-transformer-remark`,
