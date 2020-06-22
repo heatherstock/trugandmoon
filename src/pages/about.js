eef29db60b80c966454a56c27765ffaaf3c92f36
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2020-06-22 14:11:48 +0100

    Edit about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,20 +6,20 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <p>
-      Drayson and Stock is a collaboration between husband and wife team  James Drayson and Heather Stock. 
+      Trug and Moon is run by Drayson and Stock - a collaboration between husband and wife team James Drayson and Heather Stock. 
       We’re constantly striving to do what we love and learn new things, from growing our own vegetables, 
       expanding our yoga practice, traversing our spiritual awakenings and beginning to share these with the world. 
       When we're not working in pixels and lines of code as a web designer and software engineer, we're setting 
       metal and wood type in our letterpress workshop in the garden of England or running workshops on growth and 
       wellbeing.
     </p>
-    <hr></hr>
+    <p>--</p>
     <p>
       Drayson is a digital designer and letterpress printer with a passion for beautiful typography and a 
       thing for nice paper. He creates beautiful letterpress wedding stationery, greetings cards and prints 
       using antique type and printing equipment, and is usually found covered in ink or oil (or both!).
     </p> 
-    <hr></hr>
+    <p>--</p>
     <p>
       Stock is currently a software engineer following myriad other "careers" in chemistry, SCUBA diving, 
       teaching science and even teaching Mandarin Chinese to British Chinese children in Liverpool's China Town 
