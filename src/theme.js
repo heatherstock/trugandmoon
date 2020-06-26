9a5c551ae91d73d421134fe306c34c9215e3e882
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2020-06-26 16:57:42 +0100

    edit global style - media queries

--- src/theme.js
+++ src/theme.js
@@ -4,13 +4,19 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    padding: 0;
+    @media (max-width: 600px) {
+      padding: 0 18px;
+    }
+    @media (min-width: 600px) {
+      padding: 0 36px;
+    }
     margin: auto;
     font-family: Baskervville, serif;
     font-size: 16px;
     position: relative;
     min-height: 100vh;
     max-width: 1440px;
+
   }
   h1, h2, h3 {
     font-family: Work Sans, sans serif;
