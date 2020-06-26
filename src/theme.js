38592ee44633aba9e67b06cb49398fd3ab8c9f51
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2020-06-26 16:18:32 +0100

    Edit global style

--- src/theme.js
+++ src/theme.js
@@ -5,11 +5,12 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
-    margin: 0;
+    margin: auto;
     font-family: Baskervville, serif;
     font-size: 16px;
     position: relative;
     min-height: 100vh;
+    max-width: 1440px;
   }
   h1, h2, h3 {
     font-family: Work Sans, sans serif;
