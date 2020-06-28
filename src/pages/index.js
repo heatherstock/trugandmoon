d0c3b521bf6dcc0b38e12c92e6cffc18910ff221
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2020-06-28 17:17:05 +0100

    Add media queries for section 2 of home page :construction: Rest commented out for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,7 @@ const Wrapper = styled.div`
 @media (max-width: 600px) {
   grid-gap: 18px;
 }
-@media (max-width: 960px) {
-  grid-template: auto/repeat(6,1fr);
-}
   display: grid;
-  grid-gap: 30px;
   grid-template: auto/repeat(12,1fr);
   grid-auto-flow: row;
   overflow: hidden;
@@ -24,10 +20,13 @@ const Wrapper = styled.div`
 // 60rem = 960
 // 37.5rem = 600
 
+const Welcome = styled.div`
+  grid-column: span 12;
+`;
+
 const Section1 = styled.div`
   @media (max-width: 960px) {
-    grid-template: auto/repeat(6,1fr);
-    grid-column: span 6;
+    grid-column: span 12;
   }
   display: grid;
   grid-gap: 30px;
@@ -36,25 +35,14 @@ const Section1 = styled.div`
   grid-column: span 12;
 `;
 
-const Welcome = styled.div`
-@media (max-width: 960px) {
-  grid-column: span 6;
-}
-  grid-column: span 12;
-`;
-
 const Subtitle = styled.div`
-@media (max-width: 960px) {
-  grid-column: span 6;
-}
   grid-column: span 12;
   font-style: italic;
 `;
 
 const Type1Container = styled.div`
   @media (max-width: 960px) {
-    grid-template: auto/repeat(6,1fr);
-    grid-column: span 6;
+    grid-column: span 12;
   }
   grid-area: auto/1/auto/-1;
   display: grid;
@@ -83,49 +71,125 @@ const Type1Image = styled(Img)`
 const Type2Container = styled.div`
   @media (max-width: 960px) {
     display: grid;
-    grid-template: auto/repeat(6,1fr);
+    grid-template: auto/repeat(12,1fr);
     grid-gap: 30px;
     grid-auto-flow: row;
     grid-area: auto/1/auto/-1;
-    grid-column: span 6;
+    grid-column: span 12;
   }
   grid-column: span 3;
 `;
 
 const Type2Text = styled.div`
   @media (max-width: 600px) {
-    grid-column: span 6;
+    grid-column: span 12;
   }
   @media (min-width: 600px) and (max-width: 960px) {
-    grid-column: span 3;
+    grid-column: span 6;
     grid-area: 1/1/auto/-1;
-    grid-column-start: 4;
+    grid-column-start: 7;
   }
 `;
 
 const Type2Image = styled(Img)`
-@media (max-width: 600px) {
-  grid-column: span 6;
-}
+  @media (max-width: 600px) {
+    grid-column: span 12;
+  }
   @media (min-width: 600px) and (max-width: 960px) {
-    grid-column: span 3;
+    grid-column: span 6;
     grid-area: 1/1/auto/-1;
-    grid-column-end: 4;
+    grid-column-end: 7;
   }
   height: 150px;
 `;
 
-const Type3 = styled.div`
+const Type3Text = styled.div`
   grid-column: span 3;
 `;
 
-const Type4 = styled.div`
-  grid-column: span 3;
+const Section2 = styled.div`
+  @media (max-width: 960px) {
+    grid-column: span 12;
+  }
+  @media (min-width: 960px) {
+    grid-column: span 8;
+  }
+  @media (min-width: 1280px) {
+    grid-column: span 12;
+  }
+  display: grid;
+  grid-gap: 30px;
+  grid-template: auto/repeat(12,1fr);
+  grid-auto-flow: row;
+`;
+
+const Type4Container = styled.div`
+  @media (max-width: 1280px) {
+    grid-column: span 12;
+  }
+  display: grid;
+  grid-gap: 30px;
+  grid-auto-flow: row;
+  grid-template: auto/repeat(12,1fr);
+  grid-area: auto/1/auto/-1;
+  grid-column: span 9;
+`;
+
+const Type4Text = styled.div`
+@media (max-width: 1280px) {
+  grid-column: span 12;
+  grid-area: 2/1/auto/-1;
+}
+  grid-column: span 4;
 `;
 
 const Type4Image = styled(Img)`
-  grid-column: span 6;
+@media (max-width: 1280px) {
+  grid-column: span 12;
+  grid-area: 1/1/auto/-1;
+}
+  grid-column: span 8;
   height: 350px;
+`;
+
+const Type5Container = styled.div`
+  @media (max-width: 1280px) {
+    display: grid;
+    grid-gap: 30px;
+    grid-auto-flow: row;
+    grid-template: auto/repeat(12,1fr);
+    grid-column: span 12;
+  }
+  grid-column: span 3;
+`;
+
+const Type5Text = styled.div`
+@media (max-width: 600px) {
+  grid-column: span 12;
+}
+@media (min-width: 600px) and (max-width: 960px) {
+  grid-column: 7 / -1;
+  grid-row-start: 1;
+}
+  @media (min-width: 960px) and (max-width: 1280px) {
+    grid-column: 1 / 7;
+    grid-row-start: 1;
+  }
+`;
+
+const Type5Image = styled(Img)`
+@media (max-width: 600px) {
+  grid-column: span 12;
+}
+@media (min-width: 600px) and (max-width: 960px) {
+  grid-column: 1 / 7;
+  grid-row-start: 1;
+}
+@media (min-width: 960px) and (max-width: 1280px) {
+  grid-column: 7 / -1;
+  grid-row-start: 1;
+}
+  height: 150px;
 `;
 
 const Edition = styled.h1`
@@ -186,52 +250,60 @@ const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => (
           </Type2Text>
         </Type2Container>
       </Section1>
+      <Section2>
+        <Subtitle>Subtitle</Subtitle>
+        <Type4Container>
+          <Type4Text>
+          <StyledLink to={edges[2].node.fields.slug}>
+              <Article>{edges[2].node.frontmatter.title}</Article>
+            </StyledLink>
+            <p>{edges[2].node.excerpt}</p>
+            <p>{edges[2].node.frontmatter.tag}</p>
+          </Type4Text>
+          <Type4Image fluid={edges[2].node.frontmatter.featuredImage.childImageSharp.fluid} />
+        </Type4Container>
+        <Type5Container>
+          <Type5Image fluid={edges[4].node.frontmatter.featuredImage.childImageSharp.fluid} />
+          <Type5Text>
+            <StyledLink to={edges[4].node.fields.slug}>
+              <Article>{edges[4].node.frontmatter.title}</Article>
+            </StyledLink>
+            <p>{edges[4].node.excerpt}</p>
+            <p>{edges[4].node.frontmatter.tag}</p>
+          </Type5Text>
+        </Type5Container>
+      </Section2>
     </Wrapper>
-      {/* <Type3>
+    {/* <Wrapper>
+      <Type3Text>
         <StyledLink to={edges[5].node.fields.slug}>
           <Article>{edges[5].node.frontmatter.title}</Article>
         </StyledLink>
         <p>{edges[5].node.excerpt}</p>
         <p>{edges[5].node.frontmatter.tag}</p>
-      </Type3>
-      <Type3>
+      </Type3Text>
+      <Type3Text>
         <StyledLink to={edges[6].node.fields.slug}>
           <Article>{edges[6].node.frontmatter.title}</Article>
         </StyledLink>
         <p>{edges[6].node.excerpt}</p>
         <p>{edges[6].node.frontmatter.tag}</p>
-      </Type3>
-      <Type3>
+      </Type3Text>
+      <Type3Text>
         <StyledLink to={edges[7].node.fields.slug}>
           <Article>{edges[7].node.frontmatter.title}</Article>
         </StyledLink>
         <p>{edges[7].node.excerpt}</p>
         <p>{edges[7].node.frontmatter.tag}</p>
-      </Type3>
-      <Type3>
+      </Type3Text>
+      <Type3Text>
         <StyledLink to={edges[8].node.fields.slug}>
           <Article>{edges[8].node.frontmatter.title}</Article>
         </StyledLink>
         <p>{edges[8].node.excerpt}</p>
         <p>{edges[8].node.frontmatter.tag}</p>
-      </Type3>
-      <Subtitle>Subtitle</Subtitle>
-      <Type4>
-      <StyledLink to={edges[2].node.fields.slug}>
-          <Article>{edges[2].node.frontmatter.title}</Article>
-        </StyledLink>
-        <p>{edges[2].node.excerpt}</p>
-        <p>{edges[2].node.frontmatter.tag}</p>
-      </Type4>
-      <Type4Image fluid={edges[2].node.frontmatter.featuredImage.childImageSharp.fluid} />
-      <Type2> */}
-        {/* <Type2Image fluid={edges[4].node.frontmatter.featuredImage.childImageSharp.fluid} />
-        <StyledLink to={edges[4].node.fields.slug}>
-          <Article>{edges[4].node.frontmatter.title}</Article>
-        </StyledLink>
-        <p>{edges[4].node.excerpt}</p>
-        <p>{edges[4].node.frontmatter.tag}</p>
-      </Type2> */}
+      </Type3Text>
+    </Wrapper> */}
   </Layout>
 )
 
